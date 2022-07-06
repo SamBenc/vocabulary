2cd5cd06be9f85f7032f79eb6e4d9fb15ce96eeb
SamBenc <hbldev20@gmail.com>
2022-07-06 13:03:06 -0400

    mainetance

--- app/views.py
+++ app/views.py
@@ -7,18 +7,18 @@ def home():
     try:
         return render_template("index.html", extension = "typical", title = "Vocabulary", content1 = tools.show_home())
     except Exception as e:
-        return render_template("index.html", extension = "typical", title = "Error", content1 = e)
+        return render_template("index.html", extension = "typical", title = "Maintenance Required", content1 = e)
 
 @app.route("/<csv>")
 def csv(csv):
     try:
         return render_template("index.html", extension = "typical", title = csv.capitalize(), content1 = tools.show_topic(csv))
     except Exception as e:
-        return render_template("index.html", extension = "typical", title = "Error", content1 = e)
+        return render_template("index.html", extension = "typical", title = "Maintenance Required", content1 = e)
 
 @app.route("/<csv>/<topic>", methods=["GET", "POST"])
 def theme(csv, topic):
     try:
         return render_template("index.html", extension = "typical", title = f"{csv.capitalize()} > {topic.capitalize()}", content1 = tools.show_words(csv,topic), footer = f'/{csv}/{tools.next_topic(csv,topic)}')
     except Exception as e:
-        return render_template("index.html", extension = "typical", title = "Error", content1 = e)
+        return render_template("index.html", extension = "typical", title = "Maintenance Required", content1 = e)
